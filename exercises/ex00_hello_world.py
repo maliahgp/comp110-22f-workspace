f24c9bca6665327d9a58aa7ff3978dfe0d9af918
Maliah Petefish <petefishmaliah12@gmail.com>
2022-08-26 16:36:17 -0400

    Progress on ex01

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,3 +1,5 @@
-print("Hello, World")
+
 """My first program for COMP110."""
+
+print("Hello, World")
 __author__ = "730523557"
---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,34 @@
+"""Small step to wordle- EX01"""
+
+__author__ = "730523557"
+
+word_choice: str = input("Enter a 5-character word: ")
+if len(word_choice) != 5:
+    print("Error: Word must contain 5 characters.")
+    exit()
+
+character_choice: str = input("Enter a single character: ")
+if len(character_choice) != 1:
+    print("Error: Character must be a single character.")
+
+instances_of_character: int = 0 
+print("Searching for " + character_choice + " in " + word_choice) 
+if character_choice == word_choice[0]:
+    print(character_choice + " found in index 0.")
+    instances_of_character = instances_of_character + 1
+if character_choice == word_choice[1]:
+    print(character_choice + " found in index 1.")
+    instances_of_character = instances_of_character + 1
+if character_choice == word_choice[2]:
+    print(character_choice + " found in index 2.")
+    instances_of_character = instances_of_character + 1
+if character_choice == word_choice[3]:
+    print(character_choice + " found in index 3.")
+    instances_of_character = instances_of_character + 1
+if character_choice == word_choice[4]:
+    print(character_choice + " found in index 4.")
+    instances_of_character = instances_of_character + 1
+if instances_of_character > 0:
+    print(str(instances_of_character) + " instances of " + character_choice + " found in " + word_choice )
+else:
+    print("No instances of " + character_choice + " found in " + word_choice)
