b7816e0dc844ad9e596f3ddb3ac5165fff77e934
Maliah Petefish <petefishmaliah12@gmail.com>
2022-08-27 13:21:57 -0400

    Ex01 revision 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,4 +1,4 @@
-"""Small step to wordle- EX01"""
+"""Small step to wordle- EX01."""
 
 __author__ = "730523557"
 
@@ -10,6 +10,7 @@ if len(word_choice) != 5:
 character_choice: str = input("Enter a single character: ")
 if len(character_choice) != 1:
     print("Error: Character must be a single character.")
+    exit()
 
 instances_of_character: int = 0 
 print("Searching for " + character_choice + " in " + word_choice) 
@@ -28,7 +29,9 @@ if character_choice == word_choice[3]:
 if character_choice == word_choice[4]:
     print(character_choice + " found in index 4.")
     instances_of_character = instances_of_character + 1
-if instances_of_character > 0:
+if instances_of_character == 1:
+    print(str(instances_of_character) + " instance of " + character_choice + " found in " + word_choice)
+if instances_of_character > 1:
     print(str(instances_of_character) + " instances of " + character_choice + " found in " + word_choice )
-else:
+if instances_of_character == 0:
     print("No instances of " + character_choice + " found in " + word_choice)
